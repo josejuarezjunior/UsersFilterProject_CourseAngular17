a2242ce701aa2f685b372109159aab34d574884f
Jose Juarez Junior <josejuarez.junior@ziliatech.com>
2024-05-23 08:14:14 -0300

    feat: add logic to filter users list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
+import { IFilterOptions } from './interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  usersList: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.usersList = UsersList;
+    }, 1);
+  }
 
   onUserSelected(user: IUser) {
     this.userSelected = user;
     this.showUserDetails = true;
+  }
+
+  onFilter(filterOptions: IFilterOptions) {
+    console.log(filterOptions);
   }
 }
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { IFilterOptions } from '../../interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-filter',
@@ -6,14 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
-  users = [
-    {
-      value: true,
-      viewValue: 'Sim'
-    },
-    {
-      value: false,
-      viewValue: 'Não'
-    }
+  filterOptions: IFilterOptions = {
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined,
+  };
+
+  statusList = [
+    { description: 'Ativo', value: true },
+    { description: 'Inativo', value: false }
   ];
+
+  @Output('onFilter') onFilterEmitt = new EventEmitter<IFilterOptions>();
+
+  onFilter() {
+    this.onFilterEmitt.emit(this.filterOptions);
+  }
 }
