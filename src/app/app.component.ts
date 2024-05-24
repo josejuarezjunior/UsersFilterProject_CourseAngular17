c58cc05bccbc338453468b591a3000fb1998a56d
Jose Juarez Junior <josejuarez.junior@ziliatech.com>
2024-05-23 21:28:46 -0300

    feat: set logic to filter user by name and status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 import { IFilterOptions } from './interfaces/filter-options.interface';
+import { IStatus } from './interfaces/user/status.interface';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,14 @@ import { IFilterOptions } from './interfaces/filter-options.interface';
 })
 export class AppComponent implements OnInit {
   usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
 
   ngOnInit() {
     setTimeout(() => {
       this.usersList = UsersList;
+      this.usersListFiltered = this.usersList;
     }, 1);
   }
 
@@ -26,5 +29,41 @@ export class AppComponent implements OnInit {
 
   onFilter(filterOptions: IFilterOptions) {
     console.log(filterOptions);
+
+    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
   }
+
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersByName(filterOptions.name, usersList);
+    filteredList = this.filterUserByStatus(filterOptions.status, filteredList);
+
+    return filteredList;
+  }
+
+  filterUsersByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+
+    if (NAME_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.nome.toLowerCase().includes(name.toLowerCase()));
+
+    return filteredList;
+  }
+
+  filterUserByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
+    const STATUS_NOT_SELECTED = status === undefined;
+
+    if (STATUS_NOT_SELECTED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.ativo === status);
+
+    return filteredList;
+  }
+
 }
